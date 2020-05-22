ff5146932880a661e409a3984608ac4d56bc76b7
Thomas Cheng <tcheng878@gmail.com>
2020-05-22 10:31:43 -0700

    second commit

--- backups/server/models/product.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const ProductSchema = new mongoose.Schema({
-    title: { type: String},
-    description: { type: String},
-    price: { type: Number }
-}, { timestamps: true});
-
-module.exports.Product = mongoose.model('Product', ProductSchema);
--- backups/server/routes/product.routes.js
+++
@@ -1,7 +0,0 @@
-const ProductController = require('../controllers/product.controller');
-module.exports = function(app){
-    app.get('/api', ProductController.index);
-    app.post('/api/product', ProductController.createProduct);
-    app.get('/api/products', ProductController.getAllProducts);
-    app.get('/api/product/:id', ProductController.getProduct);
-}
